8032ef6eaea59569fdb571370acf55bb49983a68
miguel5219 <5219@holbertonschool.com>
2022-07-06 10:42:16 -0700

    multiplies two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * main - multiplies two numbers
+  * @argc: count
+  * @argv: array
+  * Return: 0
+  */
+
+int main( int argc, char **argv)
+{
+	int x;
+	int y;
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	mul = x * y;
+	printf("%d\n", mul);
+
+	return (0);
+}
