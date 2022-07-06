fd979ae30675047532cd48f59a3ac0d5bf3ff2f6
miguel5219 <5219@holbertonschool.com>
2022-07-06 10:27:16 -0700

    Write a program that prints all arguments it receives.

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * main - prints the number of arguments
+  * @argc: count
+  * @argv: array
+  * Return: 0
+  */
+
+int main(int argc, char **argv)
+{
+	int = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
