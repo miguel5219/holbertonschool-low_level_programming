eac25cf79282f16305389aed03b18a9005c81a35
miguel5219 <5219@holbertonschool.com>
2022-07-09 18:47:49 -0700

    concatenated two strings

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+  * string_nconcat - concatenated two strings
+  * @s1: char
+  * @s2: char
+  * @n: unsigned int
+  * Return: char
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, size1 = sizeof(s1), size2 = sizeof(s2);
+	char *p;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	if (n > size2)
+	{
+		n = size2;
+	}
+	p = malloc((size1 + n) * sizeof(char) - 2);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (j = 0; j < n && s2[j] != '\0'; j++)
+	{
+		p[j + i] = s2[j];
+	}
+	p[j + i] = '\0';
+	return (p);
+}
