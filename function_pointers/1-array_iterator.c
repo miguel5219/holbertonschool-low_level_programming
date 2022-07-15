47a4a0a983ad003a82715e82f2b5efd32a462db2
miguel5219 <5219@holbertonschool.com>
2022-07-14 21:53:30 -0700

    executes a function given as a parameter

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
-  * array_iterator -  executes a function given as a parameter 
+  * array_iterator -  executes a function given as a parameter
   * @array: pointer to int
   * @size: size bit
   * @action: pointer to function
