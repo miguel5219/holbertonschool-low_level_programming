6e92e7003038b56b8d745d2c30efa04025a17cf1
miguel5219 <5219@holbertonschool.com>
2022-07-27 16:00:38 -0700

    adds a new node at the end

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,10 +10,10 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node;
-	listint_t *p= *head;
+	listint_t *p = *head;
 
 	new_node = malloc(sizeof(listint_t));
-	if(new_node == NULL)
+	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
