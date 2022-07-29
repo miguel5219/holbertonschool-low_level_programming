1508cd96435ff316224af93da54f6d50a4329855
miguel5219 <5219@holbertonschool.com>
2022-07-29 12:50:18 -0700

    deletes the node at index index of a listint_t linked list, fix betty style

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		free (p);
+		free(p);
 		return (1);
 	}
 	for (i = 0; i < index - 1; i++)
