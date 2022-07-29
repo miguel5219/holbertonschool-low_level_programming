9748d0d31fe0f9211d8b251f68556cc2f6f31b6a
miguel5219 <5219@holbertonschool.com>
2022-07-29 12:05:03 -0700

    inserts a new node at a given position, fix kernel style code

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	for (i = 1; i <= idx - 1; i++)
 	{
-		if (p == NULL ||p->next == NULL)
+		if (p == NULL || p->next == NULL)
 		{
 			return (NULL);
 		}
