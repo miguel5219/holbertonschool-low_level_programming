2437ea6a7e6398840dead4735f0d183f9b46b69c
miguel5219 <5219@holbertonschool.com>
2022-08-03 16:39:41 -0700

    fix betty style

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,10 +13,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 		for (tmp = 0; text_content[tmp] != '\0'; tmp++)
+		{
+		}
 	
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_CREAT | O_RDWR |O_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 	wfile = write(file, text_content, tmp);
