baf200767f4a65932f19abef8593ba30b78e522e
miguel5219 <5219@holbertonschool.com>
2022-08-04 11:26:08 -0700

    fix betty style

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,13 +16,13 @@ int append_text_to_file(const  char *filename, char *text_content)
 		{
 		}
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 	file = open(filename, O_WRONLY | O_APPEND);
 	if (file == -1)
 		return (-1);
 	wfile = write(file, text_content, tmp);
 	if (wfile == -1)
 		return (-1);
-	close (file);
+	close(file);
 	return (1);
 }
