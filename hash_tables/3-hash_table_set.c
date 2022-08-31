33767356e320e2d59289ce250c6a5a3fc0432e32
miguel5219 <5219@holbertonschool.com>
2022-08-31 13:00:03 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 0-hash_table_create.c 1-djb2.c 2-key_index.c 3-hash_table_set.c -o d

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,50 @@
+#include "hash_tables.h"
+
+/**
+  * hash_tables_set - function that adds an element to the hash table
+  * @ht: isthe hash table you want to add the key/value
+  * @key: is the key
+  * @value: is the vlue associated with the key
+  * Return: index of the key
+  */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_elem, *existing;
+	unsigned long int index;
+
+	if (ht == NULL)
+		return (0);
+	if (key == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	existing = ht->array[index];
+
+	while (existing != NULL)
+	{
+		if (strcmp(existing->key, key) == 0)
+		{
+			existing->value = strdup(value);
+			return (1);
+		}
+		existing = existing->next;
+	}
+	new_elem = malloc(sizeof(hash_node_t));
+	new_elem->key = strdup(key);
+	new_elem->value = strdup(value);
+	
+	if (new_elem->value == NULL)
+		return (0);
+
+	new_elem->next = ht->array[index];
+	ht->array[index] = new_elem;
+	return (1);
+}
+
+
+
+
+
+
+
